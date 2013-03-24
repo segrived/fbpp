a3bd513981aee93c6524b4e072b58adef8e67160
Holden Cox <segrived@gmail.com>
2013-03-24 21:12:01 +0200

    remove unused files

--- app/controllers/admin/lecturer_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::LecturersController < Admin::AdminController
-    # PUT /admin/lecturers/set_confirmation_level/:lecturer_id/:level
-    def set_confirmation_level
-        lecturer = Lecurer.find(params[:user_id])
-        lecturer.confirm_level = params[:level]
-        lecturer.save
-    end
-end
