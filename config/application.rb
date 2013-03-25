0f63ea54067b98fdf06e2118d6ab2fd2d49d294f
Holden Cox <segrived@gmail.com>
2013-03-25 10:54:42 +0200

    assert

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Feedback
     config.active_record.whitelist_attributes = true
     config.assets.enabled = true
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = true
     config.i18n.default_locale = :ru
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.autoload_paths += %W(#{config.root}/extras)
