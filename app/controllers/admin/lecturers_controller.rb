673b25490d6a46ae172512dcff9eb2f7e080d026
Holden Cox <segrived@gmail.com>
2013-03-27 04:46:39 +0200

    text fix

--- app/controllers/admin/lecturers_controller.rb
+++ app/controllers/admin/lecturers_controller.rb
@@ -6,8 +6,8 @@ class Admin::LecturersController < Admin::AdminController
       lecturer.update_attributes({:confirm_level => params[:confirm_level]})
       message = PrivateMessage.new({
         :receiver_id => lecturer.user.id, :sender_id => logged_user.id,
-        :title => "Ваш аккаунт был подтверждён",
-        :body => "Статут вашего аккаунта был подтверждён (#{params[:confirm_level]})"
+        :title => "Статус аккаунта был изменен",
+        :body => "Статут вашего аккаунта был изменен"
       })
       message.save
       redirect_to :back
